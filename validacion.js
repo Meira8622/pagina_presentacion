d9389a77699c1a5fd3ab2d9c45bc96a68f961562
Meira8622 <paulomeira2000@gmail.com>
2024-04-17 03:05:39 -0300

    Fix css for tablet and phone

---
+++ validacion.js
@@ -0,0 +1,13 @@
+
+let nombre = document.getElementsByClassName('.input_nombre');
+
+let email = document.getElementsByClassName('.input_email');
+
+let asunto = document.getElementsByClassName('.input_asunto');
+
+let mensaje = document.getElementsByClassName('.textarea_mensaje');
+
+let button = document.getElementsByClassName('.formcontato__botao');
+
+button.disabled = true;
+
